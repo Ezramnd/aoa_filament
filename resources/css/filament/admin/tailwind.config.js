c9a5f1503b77e6ac247e265ac5d2615a4a47d51c
Ezra Mendrofa <ezramnd@gmail.com>
2024-10-25 12:20:18 +0700

    first commit

---
+++ resources/css/filament/admin/tailwind.config.js
@@ -0,0 +1,46 @@
+import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
+/** @type {import('tailwindcss').Config} */
+
+export default {
+    presets: [preset],
+    content: [
+        './app/Filament/**/*.php',
+        './resources/views/filament/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+    ],
+    theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'blue': '#1fb6ff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }   
+  }
+  
+}
